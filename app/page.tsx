df83268a8c71b57691f3d9787a95ede43a9e93b4
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2024-01-25 17:04:16 +0530

    fix: apply gradient to header txt

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="heading--h1">Your rooftop solar plant proposal</h1>
+      <h1 className="heading--h1 text-center text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-600">
+        Your rooftop solar plant proposal
+      </h1>
     </main>
   );
 }
