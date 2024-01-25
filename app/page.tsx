0463e06e995b3a1bde6a951a207b529dad1e8b57
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2024-01-25 17:23:21 +0530

    feat: add header text

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 export default function Home() {
   return (
-    <main>
-      <h1 className="heading--h1 text-center text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-blue-600">
-        Your rooftop solar plant proposal
-      </h1>
+    <main className="mt-5">
+      <div className="flex justify-center mb-5">
+        <div className="bg-brand-blue-100 w-fit py-2.5 px-5 rounded-[32px] flex justify-center items-center gap-x-2">
+          <span className="wave text-2xl">👋</span>
+          <span className="text-sm font-archivo">Rakesh ji</span>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <h1 className="max-w-80 heading--h1 text-center text-transparent bg-clip-text bg-gradient-to-br from-[#00568D] to-[#5BB8F4]">
+          Your rooftop solar plant proposal
+        </h1>
+      </div>
     </main>
   );
 }
