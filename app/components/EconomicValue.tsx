de02d05eb6d1442b5948ee358faebf405c2fc04f
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2024-02-01 16:13:45 +0530

    feat: complete pricing modal

--- app/components/EconomicValue.tsx
+++ app/components/EconomicValue.tsx
@@ -1,30 +1,41 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import Investment from '@images/good-investment-bg.png';
+import EconomicValueModal from './Modals/EconomicValue';
+import Modal from '../../components/Modal';
 
 const EconomicValue = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="bg-brand-green-100 font-archivo py-6 px-4 relative w-full h-[220px] mb-8">
-      <div className="relative z-30 flex flex-col gap-y-5">
-        <span className="uppercase font-light text-sm">ECONOMIC VALUE</span>
-        <h3 className="text-xl font-medium max-w-[60%]">
-          Wondering if solar is a good investment?
-        </h3>
+    <>
+      <div className="bg-brand-green-100 font-archivo py-6 px-4 relative w-full h-[220px] mb-8 z-10">
+        <div className="relative z-30 flex flex-col gap-y-5">
+          <span className="uppercase font-light text-sm">ECONOMIC VALUE</span>
+          <h3 className="text-xl font-medium max-w-[60%]">
+            Wondering if solar is a good investment?
+          </h3>
 
-        <button
-          className="rounded-full py-3 px-5 text-sm font-medium font-inter w-fit text-white mb-6"
-          style={{
-            background:
-              'linear-gradient(103deg, #5FEE86 -15.41%, #23853E 109.24%)',
-          }}
-        >
-          Read More
-        </button>
+          <button
+            className="rounded-full py-3 px-5 text-sm font-medium font-inter w-fit text-white mb-6"
+            style={{
+              background:
+                'linear-gradient(103deg, #5FEE86 -15.41%, #23853E 109.24%)',
+            }}
+            onClick={() => setIsModalOpen(true)}
+          >
+            Read More
+          </button>
+        </div>
+        <div className="absolute right-0 bottom-0">
+          <Image src={Investment} alt="" width={194} height={194} />
+        </div>
       </div>
-      <div className="absolute right-0 bottom-0">
-        <Image src={Investment} alt="" width={194} height={194} />
-      </div>
-    </div>
+      <Modal isOpen={isModalOpen} onChange={setIsModalOpen}>
+        <EconomicValueModal />
+      </Modal>
+    </>
   );
 };
 
--- app/components/Modals/LockPrice.tsx
+++ app/components/Modals/LockPrice.tsx
@@ -1,9 +1,90 @@
 import React from 'react';
+import { MdRadioButtonChecked } from 'react-icons/md';
 
-type Props = {};
+const LockPrice = () => {
+  return (
+    <>
+      <h5 className="heading-gradient text-left my-5">Lock the price</h5>
+      <div className="mb-7">
+        <div className="flex h-full">
+          <div className="h-full pt-1">
+            <MdRadioButtonChecked className="w-4 h-4" fill="#009429" />
+            <div className="flex items-center justify-center h-full">
+              <div className="w-[1px] h-[80px] bg-brand-grey-400"></div>
+            </div>
+          </div>
 
-const LockPrice = (props: Props) => {
-  return <div>LockPrice</div>;
+          <div className="ml-4 font-archivo">
+            <h2 className="text-base">Pay ₹25,000 as booking advance.</h2>
+            <p className="text-sm text-gray-600 mt-1.5">
+              Don’t worry, this amount will be adjusted in the final payment.{' '}
+            </p>
+          </div>
+        </div>
+        <div className="flex h-full">
+          <div className="h-full pt-1">
+            <MdRadioButtonChecked className="w-4 h-4" fill="#009429" />
+            <div className="flex items-center justify-center h-full">
+              <div className="w-[1px] h-[80px] bg-brand-grey-400"></div>
+            </div>
+          </div>
+
+          <div className="ml-4 font-archivo">
+            <h2 className="text-base">
+              Share payment proof with Fenice partner{' '}
+            </h2>
+            <p className="text-sm text-gray-600 mt-1.5">
+              Share screenshot of the payment and other details with your
+              assigned fenice partner.
+            </p>
+          </div>
+        </div>
+        <div className="flex h-full">
+          <div className="h-full pt-1">
+            <MdRadioButtonChecked className="w-4 h-4" fill="#009429" />
+            <div className="flex items-center justify-center h-full"></div>
+          </div>
+
+          <div className="ml-4 font-archivo">
+            <h2 className=" text-base">
+              Confirm the design and sign-off on the order.
+            </h2>
+            <p className="text-sm text-gray-600 mt-1.5">
+              Connect with your partner details and proceed with the order by
+              signing off on the design and order details.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-[10px] border border-zinc-300 border-opacity-90 flex gap-y-4 flex-col py-3 px-4">
+        <p className="text-xl font-archivo">Net Banking</p>
+        <div className="font-archivo text-base text-brand-grey-600 flex flex-col gap-y-2">
+          <p>Bank Name: HDFC Bank, R A Puram Chennai,Tamilnadu–600026</p>
+          <p>Bank Account Holder: SILRES Energy Solutions Pvt Ltd</p>
+          <p>
+            Bank Account Number: <br />
+            50200063968810
+          </p>
+          <p>IFSC Code: HDFC0000141</p>
+        </div>
+      </div>
+      <h5 className="heading-gradient text-left my-6">Cancellation policy</h5>
+
+      <ul className="px-6 flex list-disc flex-col gap-y-3 font-archivo text-brand-grey-600 text-base">
+        <li>
+          You may cancel the Order within 7 (seven) days from the date of
+          remittance of the booking advance amount.
+        </li>
+        <li>
+          No Refund after the material is dispatched for the customer’s site.
+        </li>
+        <li>
+          The price mentioned is valid for 7 days from the date of this
+          proposal.
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default LockPrice;
