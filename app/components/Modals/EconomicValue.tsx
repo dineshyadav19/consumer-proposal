91c449b77dc5e7d72bf51b338fc83250ae270c39
Dinesh <41871046+dineshyadav19@users.noreply.github.com>
2024-02-01 16:15:21 +0530

    feat: complete pricing modal (#7)

--- app/components/Modals/EconomicValue.tsx
+++ app/components/Modals/EconomicValue.tsx
@@ -1,9 +1,116 @@
 import React from 'react';
+import Header from './Header';
+import GraphImage from '@images/modal-images/evp-graph.png';
+import RustIcon from '@icons/modal-icons/Rust.svg';
 
 type Props = {};
 
-const EconomicValue = (props: Props) => {
-  return <div>EconomicValue</div>;
+const EconomicValueModal = (props: Props) => {
+  return (
+    <div>
+      <Header
+        image={GraphImage}
+        heading="Economic value"
+        subHeading="Learn more about"
+      />
+
+      <div className="flex flex-col gap-y-2">
+        <div className="py-[22px] px-2">
+          <RustIcon />
+          <p className="text-xl leading-none font-medium mt-4 mb-5">
+            First year savings
+          </p>
+          <p className="text-3xl font-semibold">₹ 20,400</p>
+        </div>
+
+        <div className="py-[22px] px-2">
+          <p className="text-xl leading-none font-medium mt-4 mb-5">
+            Return on Investement (ROI)
+          </p>
+          <div className="flex justify-between gap-x-5">
+            <div className="px-4 py-2 h-28 rounded-[10px] border border-zinc-300 border-opacity-90 flex flex-col justify-between items-start w-full">
+              <span className="max-w-24 text-base text-brand-grey-500 leading-snug">
+                With Financing
+              </span>
+              <span className="text-xl font-medium">33%</span>
+            </div>
+            <div className="green-blue-gradient p-[1px] rounded-[10px] w-full h-28">
+              <div className="px-4 py-2 bg-white rounded-[10px] flex flex-col justify-between items-start h-full">
+                <span className="max-w-24 text-base text-brand-grey-500 leading-snug">
+                  Without Financing
+                </span>
+                <span className="text-xl font-medium">33%</span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="py-[22px] px-2">
+          <p className="text-xl leading-none font-medium mt-4 mb-5">
+            Solar vs others (returns)
+          </p>
+          <div className="flex justify-between gap-x-5">
+            <div className="green-blue-gradient p-[1px] rounded-[10px] w-full h-28">
+              <div className="px-4 py-2 bg-white rounded-[10px] flex flex-col justify-between items-start h-full">
+                <span className="max-w-24 text-base text-brand-grey-500 leading-snug">
+                  Solar
+                </span>
+                <span className="text-xl font-medium">33%</span>
+              </div>
+            </div>
+            <div className="px-4 py-2 h-28 rounded-[10px] border border-zinc-300 border-opacity-90 flex flex-col justify-between items-start w-full">
+              <span className="max-w-24 text-base text-brand-grey-500 leading-snug">
+                FD
+              </span>
+              <span className="text-xl font-medium">33%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="py-[22px] px-2">
+          <p className="text-xl leading-none font-medium mt-4 mb-5">
+            Electricity tariff
+          </p>
+          <div className="flex justify-between gap-x-5">
+            <div className="green-blue-gradient p-[1px] rounded-[10px] w-full h-28">
+              <div className="px-4 py-2 bg-white rounded-[10px] flex flex-col justify-between items-start h-full">
+                <span className="max-w-24 text-base text-brand-grey-500 leading-snug">
+                  With solar
+                </span>
+                <span className="text-xl font-medium">33%</span>
+              </div>
+            </div>
+            <div className="px-4 py-2 h-28 rounded-[10px] border border-zinc-300 border-opacity-90 flex flex-col justify-between items-start w-full">
+              <span className="max-w-24 text-base text-brand-grey-500 leading-snug">
+                Without solar
+              </span>
+              <span className="text-xl font-medium">33%</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="py-[22px] px-2">
+          <p className="text-xl leading-none font-medium mb-5">
+            Payback period
+          </p>
+          <p className="text-3xl font-semibold">7 years</p>
+        </div>
+
+        <div className="flex flex-col gap-y-3 items-center justify-center text-[13px] text-brand-grey-600">
+          <p className="text-base text-stone-950">Assumptions</p>
+
+          <p className="text-center">
+            Cost of electricity over 25 years considering an escalation of 3%
+            every year
+          </p>
+
+          <p>Loan repayment period of 5 years</p>
+
+          <p>Rate of interest on loan 9.50%</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default EconomicValue;
+export default EconomicValueModal;
--- app/components/Pricing.tsx
+++ app/components/Pricing.tsx
@@ -1,144 +1,185 @@
+'use client';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { IoMdCheckmark } from 'react-icons/io';
 import Investment from '@images/good-investment-bg.png';
 import { BsArrowRightCircleFill } from 'react-icons/bs';
-import { GoArrowRight } from 'react-icons/go';
+import * as Dialog from '@radix-ui/react-dialog';
+import LockPrice from './Modals/LockPrice';
+import { GoArrowLeft } from 'react-icons/go';
 
 const Pricing = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="green-blue-gradient p-0.5 rounded-[12px]">
-      <div className="px-4 py-6 rounded-[10px] bg-white flex flex-col gap-y-5">
-        <div className="flex flex-col gap-y-5 items-center">
-          <div className="text-brand-blue-500 text-sm font-medium tracking-wide py-2 px-4 border border-brand-blue rounded-full text-center w-fit">
-            ₹2,500 discount
-          </div>
+    <>
+      <div className="green-blue-gradient p-0.5 rounded-[12px]">
+        <div className="px-4 py-6 rounded-[10px] bg-white flex flex-col gap-y-5">
+          <div className="flex flex-col gap-y-5 items-center">
+            <div className="text-brand-blue-500 text-sm font-medium tracking-wide py-2 px-4 border border-brand-blue rounded-full text-center w-fit">
+              ₹2,500 discount
+            </div>
 
-          <div className="green-blue-gradient p-0.5 rounded-full w-fit">
-            <div className="uppercase bg-black text-center px-[18px] rounded-full text-white text-2xl font-semibold font-archivo">
-              Smart
+            <div className="green-blue-gradient p-0.5 rounded-full w-fit">
+              <div className="uppercase bg-black text-center px-[18px] rounded-full text-white text-2xl font-semibold font-archivo">
+                Smart
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex flex-col items-center font-archivo">
-          <p className="green-blue-gradient text-4.5xl font-semibold text-transparent bg-clip-text ">
-            ₹ 2,55,269
-          </p>
-          <p className="text-brand-grey-500 font-semibold">
-            Total payable (incl. GST). GST
-          </p>
+          <div className="flex flex-col items-center font-archivo">
+            <p className="green-blue-gradient text-4.5xl font-semibold text-transparent bg-clip-text ">
+              ₹ 2,55,269
+            </p>
+            <p className="text-brand-grey-500 font-semibold">
+              Total payable (incl. GST). GST
+            </p>
 
-          <p className="mt-3 text-center text-base tracking-wide text-brand-grey-600">
-            EMI starting from as low as ₹1483.00 per month. Lean more
-          </p>
-        </div>
-
-        <div className="text-brand-blue-600 text-base font-medium font-archivo text-center">
-          View detailed price breakup
-        </div>
-
-        <div className="flex flex-col gap-y-2.5">
-          <div className="flex gap-x-1.5 items-center">
-            <IoMdCheckmark fill="#009429" className="font-bold w-6 h-6 p-0.5" />
-            <p className="text-lg font-medium">{'High rise structure'}</p>
-          </div>
-          <div className="inline-flex gap-x-1.5">
-            <div className="w-6 h-6 p-0.5" />
-            <p className="text-base py-1 px-4 bg-brand-green-100 rounded w-full">
-              10 year product warranty
+            <p className="mt-3 text-center text-base tracking-wide text-brand-grey-600">
+              EMI starting from as low as ₹1483.00 per month. Lean more
             </p>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-y-2.5">
-          <div className="flex gap-x-1.5 items-center">
-            <IoMdCheckmark fill="#009429" className="font-bold w-6 h-6 p-0.5" />
-            <p className="text-lg font-medium">{'Micro Inverter'}</p>
+          <div className="text-brand-blue-600 text-base font-medium font-archivo text-center">
+            View detailed price breakup
           </div>
-          <div className="inline-flex gap-x-1.5">
-            <div className="w-6 h-6 p-0.5" />
-            <p className="text-base py-1 px-4 bg-brand-green-100 rounded w-full">
-              10 year product warranty
-            </p>
-          </div>
-        </div>
 
-        <div className="flex flex-col gap-y-2.5">
-          <div className="flex gap-x-1.5 items-center">
-            <IoMdCheckmark fill="#009429" className="font-bold w-6 h-6 p-0.5" />
-            <p className="text-lg font-medium">{'Workmanship warranty'}</p>
-          </div>
-          <div className="inline-flex gap-x-1.5">
-            <div className="w-6 h-6 p-0.5" />
-            <p className="text-base py-1 px-4 bg-brand-green-100 rounded w-full">
-              5 years
-            </p>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-y-2.5">
-          <div className="flex gap-x-1.5 items-center">
-            <IoMdCheckmark fill="#009429" className="font-bold w-6 h-6 p-0.5" />
-            <p className="text-lg font-medium">
-              {'Cleaning & Maintenance visits'}
-            </p>
-          </div>
-          <div className="inline-flex gap-x-1.5">
-            <div className="w-6 h-6 p-0.5" />
-            <div className="green-blue-gradient p-0.5 rounded-[12px]">
-              <p className="text-base px-4 py-1 rounded-[10px] bg-white">
-                Quarterly health check-up & cleaning visits
+          <div className="flex flex-col gap-y-2.5">
+            <div className="flex gap-x-1.5 items-center">
+              <IoMdCheckmark
+                fill="#009429"
+                className="font-bold w-6 h-6 p-0.5"
+              />
+              <p className="text-lg font-medium">{'High rise structure'}</p>
+            </div>
+            <div className="inline-flex gap-x-1.5">
+              <div className="w-6 h-6 p-0.5" />
+              <p className="text-base py-1 px-4 bg-brand-green-100 rounded w-full">
+                10 year product warranty
               </p>
             </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-y-2.5">
-          <div className="flex gap-x-1.5 items-center">
-            <IoMdCheckmark fill="#009429" className="font-bold w-6 h-6 p-0.5" />
-            <p className="text-lg font-medium">{'Generation Guarantee'}</p>
-          </div>
-          <div className="inline-flex gap-x-1.5">
-            <div className="w-6 h-6 p-0.5" />
-            <div className="green-blue-gradient p-0.5 rounded-[12px]">
-              <p className="text-base px-4 py-1 rounded-[10px] bg-white">
-                Upgrade to PREMIUM to get 1 year of generation guarantee.
-                <br /> View PREMIUM
+          <div className="flex flex-col gap-y-2.5">
+            <div className="flex gap-x-1.5 items-center">
+              <IoMdCheckmark
+                fill="#009429"
+                className="font-bold w-6 h-6 p-0.5"
+              />
+              <p className="text-lg font-medium">{'Micro Inverter'}</p>
+            </div>
+            <div className="inline-flex gap-x-1.5">
+              <div className="w-6 h-6 p-0.5" />
+              <p className="text-base py-1 px-4 bg-brand-green-100 rounded w-full">
+                10 year product warranty
               </p>
             </div>
           </div>
-        </div>
 
-        <div className="bg-gradient-to-l from-lime-50 to-lime-200 font-archivo p-3 flex items-center relative w-full h-24 rounded-[10px]">
-          <div className="relative z-30 flex gap-y-5 justify-between items-center">
-            <h3 className="text-base tracking-wide">
-              Wondering if solar is a good investment?{' '}
-              <span className="font-medium">(ROI)</span>
-            </h3>
-
-            <div className="w-12 h-12 green-blue-gradient rounded-full stroke-none">
-              <BsArrowRightCircleFill className="w-12 h-12" fill="white" />
+          <div className="flex flex-col gap-y-2.5">
+            <div className="flex gap-x-1.5 items-center">
+              <IoMdCheckmark
+                fill="#009429"
+                className="font-bold w-6 h-6 p-0.5"
+              />
+              <p className="text-lg font-medium">{'Workmanship warranty'}</p>
+            </div>
+            <div className="inline-flex gap-x-1.5">
+              <div className="w-6 h-6 p-0.5" />
+              <p className="text-base py-1 px-4 bg-brand-green-100 rounded w-full">
+                5 years
+              </p>
             </div>
           </div>
-          <div className="absolute right-0 bottom-0">
-            <Image src={Investment} alt="" width={110} height={92} />
+
+          <div className="flex flex-col gap-y-2.5">
+            <div className="flex gap-x-1.5 items-center">
+              <IoMdCheckmark
+                fill="#009429"
+                className="font-bold w-6 h-6 p-0.5"
+              />
+              <p className="text-lg font-medium">
+                {'Cleaning & Maintenance visits'}
+              </p>
+            </div>
+            <div className="inline-flex gap-x-1.5">
+              <div className="w-6 h-6 p-0.5" />
+              <div className="green-blue-gradient p-0.5 rounded-[12px]">
+                <p className="text-base px-4 py-1 rounded-[10px] bg-white">
+                  Quarterly health check-up & cleaning visits
+                </p>
+              </div>
+            </div>
           </div>
+
+          <div className="flex flex-col gap-y-2.5">
+            <div className="flex gap-x-1.5 items-center">
+              <IoMdCheckmark
+                fill="#009429"
+                className="font-bold w-6 h-6 p-0.5"
+              />
+              <p className="text-lg font-medium">{'Generation Guarantee'}</p>
+            </div>
+            <div className="inline-flex gap-x-1.5">
+              <div className="w-6 h-6 p-0.5" />
+              <div className="green-blue-gradient p-0.5 rounded-[12px]">
+                <p className="text-base px-4 py-1 rounded-[10px] bg-white">
+                  Upgrade to PREMIUM to get 1 year of generation guarantee.
+                  <br /> View PREMIUM
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="bg-gradient-to-l from-lime-50 to-lime-200 font-archivo p-3 flex items-center relative w-full h-24 rounded-[10px] z-10">
+            <div className="relative z-30 flex gap-y-5 justify-between items-center">
+              <h3 className="text-base tracking-wide">
+                Wondering if solar is a good investment?{' '}
+                <span className="font-medium">(ROI)</span>
+              </h3>
+
+              <div className="w-12 h-12 green-blue-gradient rounded-full stroke-none">
+                <BsArrowRightCircleFill className="w-12 h-12" fill="white" />
+              </div>
+            </div>
+            <div className="absolute right-0 bottom-0">
+              <Image src={Investment} alt="" width={110} height={92} />
+            </div>
+          </div>
+
+          <p className="text-brand-grey-500 text-base text-center">
+            Discounts valid till 24th January only, don’t miss, lock the price
+            now
+          </p>
+
+          <button
+            onClick={() => setIsModalOpen(!isModalOpen)}
+            className="p-5 text-xl text-white font-medium leading-none bg-gradient-to-r from-sky-700 via-sky-500 to-sky-700 rounded-[41px] shadow-inner border border-white border-opacity-50"
+          >
+            Lock this price @ ₹25,000
+          </button>
+
+          <p className="text-base leading-none text-brand-blue-600 text-center">
+            Explore financing options
+          </p>
         </div>
-
-        <p className="text-brand-grey-500 text-base text-center">
-          Discounts valid till 24th January only, don’t miss, lock the price now
-        </p>
-
-        <button className="p-5 text-xl text-white font-medium leading-none bg-gradient-to-r from-sky-700 via-sky-500 to-sky-700 rounded-[41px] shadow-inner border border-white border-opacity-50">
-          Lock this price @ ₹25,000
-        </button>
-
-        <p className="text-base leading-none text-brand-blue-600 text-center">
-          Explore financing options
-        </p>
       </div>
-    </div>
+      <Dialog.Root open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-brand-grey-400/90  fixed inset-0" />
+          <Dialog.Content className="w-full h-full fixed top-0 bg-white overflow-scroll no-scrollbar px-4 py-6 z-20">
+            <div>
+              <div
+                className="w-12 h-12 bg-neutral-50 rounded-full border border-zinc-300 border-opacity-90 flex justify-center items-center"
+                onClick={() => setIsModalOpen(false)}
+              >
+                <GoArrowLeft className="w-8 h-8" />
+              </div>
+              <LockPrice />
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </>
   );
 };
 
