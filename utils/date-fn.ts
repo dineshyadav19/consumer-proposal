12bd0238fd90142cd8ddcbb3e507367d2aefb6e4
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2024-02-23 16:33:17 +0530

    fix: add date param

---
+++ utils/date-fn.ts
@@ -0,0 +1,37 @@
+export function getDaySuffix(day: any) {
+  if (day < 1 || day > 31) {
+    throw new Error('Invalid day');
+  }
+
+  if (day >= 11 && day <= 13) {
+    return 'th';
+  }
+
+  switch (day % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
+}
+
+export const getProposalDates = (date: Date) => {
+  const today = new Date(date);
+  const futureDate = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+  const day = futureDate.getDate();
+  const month = futureDate.toLocaleString('default', { month: 'long' });
+  const futureFormattedDate = `${day}${getDaySuffix(day)} ${month}`;
+
+  const generatedOn = `${today.getDate()}${getDaySuffix(
+    today.getDate()
+  )} ${today.toLocaleDateString('default', { month: 'long' })}`;
+
+  return {
+    generatedOn,
+    futureFormattedDate,
+  };
+};
