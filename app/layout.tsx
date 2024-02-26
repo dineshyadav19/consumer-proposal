249dab7b75cfbc5a7616d030545c918e02fd59ae
Dinesh <41871046+dineshyadav19@users.noreply.github.com>
2024-02-26 13:39:08 +0530

    fix: previw link (#14)

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,25 @@ export const metadata: Metadata = {
   title: 'Consumer Web Proposal',
   description: 'Fenice Web Proposal',
   openGraph: {
-    images: 'https://proposal.feniceenergy.com/public/images/preview.png',
+    title: 'Fenice Consumer Proposal',
+    description: 'Fenice Energy',
+    url: 'https://proposal.feniceenergy.com/',
+    siteName: 'Feniceenergy',
+    images: [
+      {
+        url: 'https://proposal.feniceenergy.com/public/images/preview.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://proposal.feniceenergy.com/public/images/preview.png',
+        width: 1800,
+        height: 1600,
+        alt: 'Consumer proposal',
+      },
+    ],
+    locale: 'en_in',
+    type: 'website',
   },
 };
 
