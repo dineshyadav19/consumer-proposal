2e00e0b1d581f851f05cbadf1d13cd203bee7598
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2024-02-27 16:33:53 +0530

    fix: add suspense boundary

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -6,8 +6,10 @@ type ContainerProps = {
 
 export default function Container({ children }: ContainerProps) {
   return (
-    <div className="max-w-[640px] mx-auto bg-white min-h-screen flex flex-col">
-      {children}
-    </div>
+    <Suspense>
+      <div className="max-w-[640px] mx-auto bg-white min-h-screen flex flex-col">
+        {children}
+      </div>
+    </Suspense>
   );
 }
