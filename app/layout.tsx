0832eb6cdd946111a2cd3d6d62cedd08f045261e
Dinesh <41871046+dineshyadav19@users.noreply.github.com>
2024-02-27 15:53:29 +0530

    Fix/app preview (#15)
    * test1: app preview
    
    * test2: app preview
    
    * test3: app preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,27 +13,16 @@ export const archivo = Archivo({
 export const metadata: Metadata = {
   title: 'Consumer Web Proposal',
   description: 'Fenice Web Proposal',
-  openGraph: {
-    title: 'Fenice Consumer Proposal',
-    description: 'Fenice Energy',
-    url: 'https://proposal.feniceenergy.com/',
-    siteName: 'Feniceenergy',
-    images: [
-      {
-        url: 'https://proposal.feniceenergy.com/public/images/preview.png',
-        width: 800,
-        height: 600,
-      },
-      {
-        url: 'https://proposal.feniceenergy.com/public/images/preview.png',
-        width: 1800,
-        height: 1600,
-        alt: 'Consumer proposal',
-      },
-    ],
-    locale: 'en_in',
-    type: 'website',
-  },
+  // metadataBase: new URL('https://proposal.feniceenergy.com/'),
+  // openGraph: {
+  //   title: 'Fenice Consumer Proposal',
+  //   description: 'Fenice Energy',
+  //   url: 'https://proposal.feniceenergy.com/',
+  //   siteName: 'Feniceenergy',
+  //   images: ['/public/images/preview.png'],
+  //   locale: 'en_in',
+  //   type: 'website',
+  // },
 };
 
 export default function RootLayout({
