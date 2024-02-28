26e0c09a36aaa9d5b974a95570a8b7d2b7eac4e3
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2024-02-28 12:23:16 +0530

    feat: add hotjar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Archivo, Inter } from 'next/font/google';
 import Container from '@components/Container';
 import '@styles/main.css';
 import Header from '@components/Header';
+import Script from 'next/script';
 
 export const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 export const archivo = Archivo({
@@ -60,6 +61,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          id="hotjar"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+             (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3884463,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+          }}
+        ></Script>
+      </head>
       <body className={inter.className}>
         <Container>
           <Header />
