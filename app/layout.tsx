33861fe2634bff1a2155fab737601e45eefb3393
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2024-03-14 10:56:16 +0530

    fix: clarity id

--- app/layout.tsx
+++ app/layout.tsx
@@ -63,23 +63,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <Script
-          id="hotjar"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-             (function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:3884463,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-            `,
-          }}
-        ></Script>
-        <Script
-          id="clarity"
+          id="ms_clarity"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
